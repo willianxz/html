0db39d35bc158f85311707e438757484c06efdea
Willian Duarte <willian_xzz@hotmail.com>
2018-08-28 20:03:50 -0300

    Primeiro commit do meu repositorio html

---
+++ iframes/incluirEmPag.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        
+        <h1><a href="http://www.w3schools.com">W3schools</a></h1>
+        <iframe src="http://www.w3schools.com" width="100%" height="500px">
+            <p>Your browser does not support iframes.</p>
+        </iframe>
+        
+         <h1><a href="http://www.duolingo.com/">Duolingo</a></h1>
+        <iframe src="http://www.duolingo.com/" width="100%" height="500px">
+            <p>Your browser does not support iframes.</p>
+        </iframe>
+        
+         <h1><a href="https://dbzportal.blogspot.com.br/p/episodiosdbsuper.html">Dragonball Super</a></h1>
+        <iframe src="https://dbzportal.blogspot.com.br/p/episodiosdbsuper.html" width="100%" height="500px">
+            <p>Your browser does not support iframes.</p>
+        </iframe>
+    </body>
+</html>
---
+++ templatesProntos/templateImagem1.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  
+  <style>
+      
+      body{
+          padding-left: 5px;
+      }
+      
+      img{
+           width: 100%;
+           padding:10px;
+      }
+    
+      #fotoPequena{
+          height: 200px;
+      }
+      #fotoPequena img{
+          width: 100%;
+      }
+      
+      #fotoGrande img{
+         width: 100%;
+         height: 100%;
+         
+      }
+      
+      h1{
+          text-align: center;
+          text-shadow: 20px 5px 15px black;
+      }
+      
+      p{
+          text-align: left;
+          margin:5px;
+      }
+      
+  </style>
+  
+</head>
+<body>
+    
+   <div class="row">
+    <ul class="nav nav-tabs">
+    <li role="presentation" class="active"><a href="#">Home</a></li>
+    <li role="presentation"><a href="#">Profile</a></li>
+    <li role="presentation"><a href="#">Messages</a></li>
+  </ul>
+   </div>
+    
+   <div class="row">
+       
+   <div class="col-sm-4" style="border-style: solid;" id="fotoGrande">
+   <img src="img/4.jpg"/>
+   
+   <h1>Coala Lectus</h1>
+   <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+    Vivamus auctor quam non mauris elementum efficitur. 
+    Pellentesque in ligula pharetra quam gravida porttitor sed et lectus.
+    Vivamus in nunc non lacus rhoncus suscipit vitae sed diam. Integer aliquet 
+    feugiat lorem a luctus. Proin tristique dui ut risus accumsan,
+    ut dignissim leo eleifend. Suspendisse potenti. Etiam leo velit,
+    scelerisque vitae sagittis pharetra, malesuada condimentum purus. 
+    In ut tincidunt nisi.   
+   </p>
+   
+   <p>
+      Curabitur pretium magna vel sollicitudin accumsan. 
+      Nulla nec nulla vel massa malesuada interdum. 
+      Nam ultrices imperdiet turpis, sit amet rutrum dui condimentum quis. 
+      Suspendisse leo dui, placerat in mi et, consectetur venenatis purus.
+      Praesent vitae consectetur magna, eu iaculis risus. Nam at scelerisque mi. 
+      Nam imperdiet, lectus vitae eleifend semper, enim erat efficitur nisi, 
+      ut fringilla nisi ligula eu lorem. Ut vestibulum nisl nec nulla facilisis dictum. 
+      Integer placerat posuere tincidunt. Cras ullamcorper leo vestibulum, auctor ipsum eget,
+      posuere mi. In ut tortor nec dui pretium laoreet sed viverra quam. 
+      Maecenas vitae blandit nisl. Nullam vitae imperdiet nisi, a fermentum velit. 
+      Mauris non tortor sit amet turpis suscipit facilisis nec ornare turpis. 
+   </p>
+   
+  
+   
+   </div>
+       
+   <div class="col-sm-2" style="border-style: solid;"><img src="img/coala1.jpg" id="fotoPequena"/></div>
+   <div class="col-sm-2" style="border-style: solid;"><img src="img/coala2.jpg" id="fotoPequena"/></div>
+   <div class="col-sm-2" style="border-style: solid;"><img src="img/coala3.jpg" id="fotoPequena"/></div>
+   <div class="col-sm-2" style="border-style: solid;"><img src="img/coala4.jpg" id="fotoPequena"/></div>
+   
+    
+   <div class="col-sm-2" style="border-style: solid;"><img src="img/coala5.jpg" id="fotoPequena"/></div>
+   <div class="col-sm-2" style="border-style: solid;"><img src="img/coala6.jpg" id="fotoPequena"/></div>
+   <div class="col-sm-2" style="border-style: solid;"><img src="img/coala7.jpg" id="fotoPequena"/></div>
+   <div class="col-sm-2" style="border-style: solid;"><img src="img/coala8.jpg" id="fotoPequena"/></div>
+   
+   
+   <div class="col-sm-2" style="border-style: solid;"><img src="img/coala9.jpg" id="fotoPequena"/></div>
+   <div class="col-sm-2" style="border-style: solid;"><img src="img/coala10.jpg" id="fotoPequena"/></div>
+   <div class="col-sm-2" style="border-style: solid;"><img src="img/coala11.jpg" id="fotoPequena"/></div>
+   <div class="col-sm-2" style="border-style: solid;"><img src="img/coala12.jpg" id="fotoPequena"/></div>
+   
+   
+   <div class="col-sm-2" style="border-style: solid;"><img src="img/coala13.jpg" id="fotoPequena"/></div>
+   <div class="col-sm-2" style="border-style: solid;"><img src="img/coala14.jpg" id="fotoPequena"/></div>
+   <div class="col-sm-2" style="border-style: solid;"><img src="img/coala15.jpg" id="fotoPequena"/></div>
+   <div class="col-sm-2" style="border-style: solid;"><img src="img/coala16.jpg" id="fotoPequena"/></div>
+   
+  
+   </div>
+
+   
+    
+    
+    
+    
+   <div class="row">
+    <ul class="nav nav-tabs">
+    <li role="presentation" class="active"><a href="#">Home</a></li>
+    <li role="presentation"><a href="#">Profile</a></li>
+    <li role="presentation"><a href="#">Messages</a></li>
+  </ul>
+   </div>
+
+</body>
+</html>
